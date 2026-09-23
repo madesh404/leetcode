61aba1d37a9b0a3d4ffc300999cd03296cf493df
Madesh Arulananthan <arulmadesh1457@gmail.com>
2026-09-22 23:56:21 -0400

    Add brute force solution for two-sum problem

--- Blind75/twoSum.cpp
+++ Blind75/twoSum.cpp
@@ -1,1 +1,16 @@
-
+// Brute Force Approach
+// For each element in nums (x), loop through nums and find if there is another element = target - x
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        // Return an empty vector if no solution is found
+        return {};
+    }
+};
